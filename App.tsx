044a7537b36162912bc60e19e12d136a19688099
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-06-16 22:18:53 -0300

    Create Theme

--- App.tsx
+++ App.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/Home';
+import { ThemeProvider } from 'styled-components/native';
+import Theme from './src/Theme';
 
 export default function App() {
   return (
-    <Home />
+    <ThemeProvider theme={Theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
---
+++ src/Theme/index.ts
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    background: "#10141b",
+    surface800: "#242633",
+
+    gryffindor: "#732D2D",
+    slytherin: "#094036",
+    ravenclaw: "#1B3659",
+    hufflePuff: "#8C5818",
+  }
+}
