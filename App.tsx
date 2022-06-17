9164518d6335b80e766adf5b9b6ed7bed7321f67
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-06-17 15:00:37 -0300

    Remove useless imports

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/Home';
 import { ThemeProvider } from 'styled-components/native';
+import Home from './src/screens/Home';
 import Theme from './src/Theme';
 
 export default function App() {
