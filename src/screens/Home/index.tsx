de5f572924147a1a6c1ca5c8188bd419eb585f48
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-06-16 23:03:26 -0300

    Adding search

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,32 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, NativeSyntheticEvent, TextInputChangeEventData, View } from 'react-native';
 import CharacterCard from '../../components/CharacterCard';
+import SearchInput from '../../components/SearchInput';
 import { Character } from '../../model/Character';
 import { getCharacters } from '../../service/CharacterService';
 
-// import { Container } from './styles';
+import { CharactersList, Container, Header, Title } from './styles';
 
 const Home = () => {
+  const [allcharacters, setAllCharacters] = useState<Character[]>([]);
   const [characters, setCharacters] = useState<Character[]>([]);
+  const [searchName, setSearchName] = useState("");
 
   useEffect(() => {
     getCharacters()
       .then(res => {
+        setAllCharacters(res.data)
         setCharacters(res.data)
       })
       .catch(err => {
         alert(err.message)
       });
-  }, [])
+  }, []);
+
+  const handleSearch = (value: string) => {
+    setSearchName(value)
+
+    setCharacters(
+      allcharacters.filter(c => c.name.includes(value))
+    )
+  }
 
   return (
-    <View>
+    <Container>
+      <Header>
+        <Title>Harry Potter's Characters!</Title>
+        <SearchInput 
+          placeholder="Insert name..."
+          value={searchName}
+          onChangeText={handleSearch}
+        />
+      </Header>
       <FlatList
-        data={characters}
+        numColumns={3}
+        data={characters.slice(0, 52)}
         keyExtractor={(character, i) => character.name+i}
         renderItem={({ item }) => <CharacterCard character={item} />}
       />
-    </View>
+    </Container>
   );
 }
 
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,32 @@
+import { FlatList } from "react-native";
+import styled from "styled-components/native";
+import { Character } from "../../model/Character";
+
+export const Header = styled.View`
+  height: 200px;
+  padding: 30px;
+
+  background-color: ${({ theme }) => theme.colors.surface900};
+  margin-bottom: 4px;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+  margin-bottom: 34px;
+`;
+
+export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const CharactersList: new <Character>() => FlatList<Character> = styled(FlatList)`
+  flex-direction: row;
+  flex-wrap: wrap;
+  background-color: red;
+` as any;
