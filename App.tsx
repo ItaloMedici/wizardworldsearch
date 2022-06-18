641fde624b867422f208b9fd906ba5da3243023f
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-06-17 23:37:02 -0300

    Applying Routes on root App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
+import Routes from './src/routes';
 import Home from './src/screens/Home';
 import Theme from './src/Theme';
 
 export default function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <Home />
+      <Routes />
     </ThemeProvider>
   );
 }
